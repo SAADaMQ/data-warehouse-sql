8af7b566c93e49200da61db4823f55168b0e77e1
saad_MQ <144943692+SAADaMQ@users.noreply.github.com>
2026-05-22 18:47:58 +0300

    Create __init__database.sql

---
+++ scipts/__init__database.sql
@@ -0,0 +1,12 @@
+use DataWareH
+
+--i already create the database and use it ! 
+--creating the schemas , brone ,silver and gold because i will use Medallion Architecture as data architecture
+create schema bronze
+go
+
+create schema silver
+go
+
+create schema gold
+go
